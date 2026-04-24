48c7535bd49f82c8234ea6b2478ba78f4308fff3
Mohaniish2208 <mohaniish5@gmail.com>
2026-04-24 04:35:10 -0400

    Added summary.

--- src/components/Layouts/PageHero.tsx
+++ src/components/Layouts/PageHero.tsx
@@ -6,6 +6,10 @@ export default function PageHero() {
       <div className="punch-line-section">
         <p className="punch-line">Let me brighten things up</p>
         <p className="for-you">for you!</p>
+        <p className="summary">
+          With a focus on trust, craftsmanship, and detail, we transform Toronto homes and commercial spaces into
+          polished, vibrant environments that stand the test of time.
+        </p>
       </div>
     </div>
   )
