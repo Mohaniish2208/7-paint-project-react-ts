f10785c6030519f5e07c8ff2056404b25339baa7
Mohaniish2208 <mohaniish5@gmail.com>
2026-05-02 14:23:40 -0400

    Added images for services section.

--- src/components/Layouts/PageHero.tsx
+++ src/components/Layouts/PageHero.tsx
@@ -15,18 +15,23 @@ export default function PageHero() {
       <div className="trust-bar-container">
         <div className="trust-bar">
           <div className="child">
+            <img className="exp-icon" src="./experience.svg" alt="exp-icon" />
             <span>20+ years experience</span>
           </div>
           <div className="child">
+            <img className="estimate-icon" src="./estimates.svg" alt="estimate-icon" />
             <span>Free Estimates</span>
           </div>
           <div className="child">
+            <img className="locations-icon" src="./locations.svg" alt="locations-icon" />
             <span>East and Central Region</span>
           </div>
           <div className="child">
+            <img className="material-icon" src="./material.svg" alt="material-icon" />
             <span>Quality Materials</span>
           </div>
           <div className="child-last">
+            <img className="references-icon" src="./references.svg" alt="references-icon" />
             <span>References Available</span>
           </div>
         </div>
