acaf5649154e0ac6c71425b2392e3aafd7927df0
Mohaniish2208 <mohaniish5@gmail.com>
2026-05-03 17:03:51 -0400

    Changed the puchline.

--- src/components/Layouts/PageHero.tsx
+++ src/components/Layouts/PageHero.tsx
@@ -4,12 +4,12 @@ export default function PageHero() {
   return (
     <div className="page-hero-container">
       <div className="punch-line-section">
-        <p className="punch-line">Let me brighten things up</p>
+        <p className="punch-line">Let us brighten things up</p>
         <p className="for-you">for you!</p>
         <p className="summary">
-          With a focus on trust, craftsmanship, and detail, we transform Toronto homes and commercial spaces into
-          polished, vibrant environments that stand the test of time. Painting across the Greater Toronto Area, backed
-          by 20+ years of experience.
+          With a focus on trust, craftsmanship, and detail, we transform Toronto homes and commercial spaces into fresh,
+          vibrant environments that stand the test of time. Painting across the Greater Toronto Area, backed by 20+
+          years of experience.
         </p>
       </div>
       <div className="trust-bar-container">
