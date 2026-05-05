63764ac81d1bf9604f9f4b8461990023d3ad60b5
Mohaniish2208 <mohaniish5@gmail.com>
2026-05-05 03:13:16 -0400

    Styling and content modified.

--- src/components/Layouts/Footer.tsx
+++ src/components/Layouts/Footer.tsx
@@ -1,10 +1,12 @@
+import "../../../src/components/styles/footer.css"
+
 export default function Footer() {
   return (
-    <div>
+    <div className="contact-container">
       <h1 className="title">Lets talk!</h1>
-      <p className="phone">Phone: (647)861-0192</p>
-      <p className="email">Email: seheight@gmail.com</p>
-      <p className="address">Address: 1500 Woodbine Avenue #30025, Toronto, Ontario. M4C 5J2</p>
+      <p className="business-phone">Phone: (647)861-0192</p>
+      <p className="business-email">Email: seheight@gmail.com</p>
+      <p className="business-address">Address: 1500 Woodbine Avenue #30025, Toronto, Ontario. M4C 5J2</p>
     </div>
   )
 }
