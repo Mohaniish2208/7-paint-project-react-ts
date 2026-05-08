cc4a918f26728d4fa0ed3af959b8489262433ef1
Mohaniish2208 <mohaniish5@gmail.com>
2026-05-08 06:19:27 -0400

    Updated 'services.tsx' as well as 'services.css' files to suit the new look.

--- src/components/Sections/services/Services.tsx
+++ src/components/Sections/services/Services.tsx
@@ -1,71 +1,57 @@
+import { gsap } from "gsap"
 import "../../styles/services.css"
+import { ScrollTrigger } from "gsap/ScrollTrigger"
+
+gsap.registerPlugin(ScrollTrigger)
 
 export default function Services() {
   const services = [
     {
       title: "House Painting",
       icon: "./house.svg",
-      image: "/service-thumbnails/house-painting.png",
+      image: "../../../../public/service-thumbnails/house-painting.jpg",
     },
     {
       title: "Condos and Apartments",
       icon: "./condos.svg",
-      image: "/services-thumbnails/condos-and-apartments.png",
+      image: "../../../../public/service-thumbnails/condos-and-apartments.png",
     },
     {
       title: "Multi-unit Buildings",
       icon: ".multi-units.svg/",
-      image: "/services-thumbnails/multi-unit-buildings.jpg",
+      image: "../../../../public/service-thumbnails/multi-unit-buildings.jpg",
     },
     {
       title: "Realtor Listing Refreshes",
       icon: "./realtor.svg",
-      image: "/services-thumbnails/realtor-listing-refreshes.png",
+      image: "../../../../public/service-thumbnails/realtor-listing-refreshes.png",
     },
     {
       title: "Select Commercial Projects",
       icon: "./commercial.svg",
-      image: "/services-thumbnails/select-commercial-projects.png",
+      image: "../../../../public/service-thumbnails/select-commercial-projects.png",
     },
     {
       title: "Free Colour Consulting",
       icon: "./colors.svg",
-      image: "/services-thumbnails/free-color-consulting.png",
+      image: "../../../../public/service-thumbnails/free-colour-consulting.png",
     },
   ]
 
   return (
     <div className="services-container">
+      <p className="services-label">Our Services</p>
+      <h2 className="services-header">Quality Painting for Every Space</h2>
       <div className="service-bar">
-        <div className="services-child">
-          <img className="house-icon" src="./house.svg" alt="house-icon" />
-          <span>House Painting</span>
-        </div>
-
-        <div className="services-child">
-          <img className="condos-icon" src="./condos.svg" alt="condos-icon" />
-          <span>Condos and Apartments</span>
-        </div>
-
-        <div className="services-child">
-          <img className="multi-units-icon" src="./multi-units.svg" alt="multi-units" />
-          <span>Multi-Unit Buildings</span>
-        </div>
-
-        <div className="services-child">
-          <img className="realtor-icon" src="./realtor.svg" alt="realtor-icon" />
-          <span>Realtor Listing Refreshes</span>
-        </div>
-
-        <div className="services-child">
-          <img className="commercial-icon" src="./commercial.svg" alt="commercial-icon" />
-          <span>Select Commercial Projects</span>
-        </div>
-
-        <div className="services-child-last">
-          <img className="colors-icon" src="./colors.svg" alt="colors-icon" />
-          <span>Free Colour Consulting</span>
-        </div>
+        {services.map((service) => (
+          <div className="services-child" key={service.title} style={{ backgroundImage: `url("${service.image}")` }}>
+            <div className="service-content">
+              <img className="service-icon" src={service.icon} alt="service-icon" />
+              <span>{service.title}</span>
+              <div className="service-accent" />
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   )
